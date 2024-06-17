763210ae930ffff7b7403c4b0161786de84bee1e
JasonRALIJAONA <jasonralijaona@gmail.com>
2024-06-17 05:06:40 -0900

    best_fit

--- 1D-packing/model/Algorithme.py
+++ 1D-packing/model/Algorithme.py
@@ -30,4 +30,35 @@ class Algorithme:
         return bacs
 
 
+    def best_fit (objets , taille_bac):
+        bacs=[]
+
+        # ajout du premier bac
+        id_bac=0
+        bacs.append(Bac(id_bac,taille_bac))
+        id_bac+=1
+
+        for objet in objets:
+            bac_choisi=None
+
+            for bac in bacs:
+                if bac.taille_restante() - objet.taille >= 0:
+                    if bac_choisi is None:
+                        bac_choisi=bac
+                    elif bac_choisi.taille_restante() - objet.taille > bac.taille_restante() - objet.taille:
+                        bac_choisi=bac
+
+            if bac_choisi is None:
+                nouveau_bac=Bac(id_bac,taille_bac)
+                nouveau_bac.objets.append(objet)
+                bacs.append(nouveau_bac)
+                objet.id_bac=nouveau_bac.id
+                id_bac+=1
+            else:
+                bac_choisi.objets.append(objet)
+                objet.id_bac=bac_choisi.id
+
+        return bacs
+                        
+
         
