6b7f1b6a6dc4ef614649b2a403f5e61910b51151
JasonRALIJAONA <jasonralijaona@gmail.com>
2024-06-24 06:25:42 -0900

    feat: creation de la fonction brut_force dans algorithm

--- 1D-packing/App.py
+++ 1D-packing/App.py
@@ -1,23 +1,22 @@
 from util.Objet import Objet
 from model.Algorithme import Algorithme
+from itertools import permutations
 
 # cree 10 objet de taille 1
 objets=[]
-objets.append(Objet(0,1))
-objets.append(Objet(1,2))
-objets.append(Objet(2,5))
-objets.append(Objet(3,1))
-objets.append(Objet(4,1))
-objets.append(Objet(5,1))
-objets.append(Objet(6,1))
-objets.append(Objet(7,1))
-objets.append(Objet(8,1))
-objets.append(Objet(9,1))
+objets.append(Objet(0,10))
+objets.append(Objet(1,20))
+objets.append(Objet(2,15))
+objets.append(Objet(3,25))
+objets.append(Objet(4,30))
 
-taille_bac=5
+taille_bac=50
 
-bacs=Algorithme.best_fit(objets,taille_bac)
+# for p in perm:
+#     print(f"[{', '.join([str(o.id) for o in p])}]")
 
+
+bacs=Algorithme.brut_force(objets,taille_bac)
 
 # for bac in bacs:
 #     print("Bac ",bac.id," : ",[objet.id for objet in bac.objets])
