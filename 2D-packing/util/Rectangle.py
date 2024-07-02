94092631a9531013f90185c13c305209754d632b
JasonRALIJAONA <jasonralijaona@gmail.com>
2024-07-01 20:35:36 -0900

    feat : ajout de couleur dans la classe rectangle

--- 2D-packing/util/Rectangle.py
+++ 2D-packing/util/Rectangle.py
@@ -1,3 +1,5 @@
+import random
+
 class Rectangle:
     def __init__(self,id ,width, height):
         self.id = id
@@ -5,5 +7,7 @@ class Rectangle:
         self.height = height
         self.pos_x = -1
         self.pos_y = -1
+        # a random color
+        self.color = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
 
     
