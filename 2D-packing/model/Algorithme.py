3ee5b481c5f144c66337bbbc37a9be15ca73853e
JasonRALIJAONA <jasonralijaona@gmail.com>
2024-07-02 03:46:54 -0900

    feat : best_fit_dh

--- 2D-packing/model/Algorithme.py
+++ 2D-packing/model/Algorithme.py
@@ -13,7 +13,7 @@ class Algorithme:
         futur_etage_y=0
         for i in range (len(rect)):
             if rect[i].height <= socle.height and rect[i].width <= socle.width:
-                if (i==0):
+                if (len(socle.etages)==0):
                     futur_etage_y=rect[i].height
                     socle.etages.append(Socle(socle.pos_x,socle.pos_y_actuel,socle.width,rect[i].height))
 
@@ -38,7 +38,7 @@ class Algorithme:
 
         for i in range(len(rect)):
             if rect[i].height <= socle.height and rect[i].width <= socle.width:
-                if (i==0):
+                if (len(socle.etages)==0):
                     futur_etage_y=rect[i].height
                     socle.etages.append(Socle(socle.pos_x,socle.pos_y_actuel,socle.width,rect[i].height))
 
@@ -57,7 +57,36 @@ class Algorithme:
                         temp.add_rect(rect[i])
                         break
 
-    
+    def best_fit_dh(self, rectangles, socle):
+        rect = self.sort_by_height(rectangles)
+        futur_etage_y = 0
+
+        for i in range(len(rect)):
+            if rect[i].height <= socle.height and rect[i].width <= socle.width:
+                if (len(socle.etages)==0):
+                    futur_etage_y=rect[i].height
+                    socle.etages.append(Socle(socle.pos_x,socle.pos_y_actuel,socle.width,rect[i].height)) 
+
+                # id de l'etage et le width restant
+                etage_choisi=(0,float("inf"))
+
+                for j in range(len(socle.etages)):
+                    etage=socle.etages[j]
+                    width_restant, height_restant = etage.espace_restant()
+
+                    if (rect[i].width <= width_restant and (width_restant-rect[i].width)<etage_choisi[1]):
+                        etage_choisi=(j,width_restant-rect[i].width)
+                
+                    elif (rect[i].height <= socle.hauteur_etage_restant() and j==len(socle.etages)-1) and etage_choisi[1]==float("inf"):
+                        socle.pos_y_actuel += futur_etage_y
+                        futur_etage_y = rect[i].height
+                        socle.etages.append(Socle(socle.pos_x , socle.pos_y_actuel, socle.width , rect[i].height))
+                        temp=socle.etages[-1]
+                        temp.add_rect(rect[i])
+                        break
+
+                if etage_choisi[1]!=float("inf"):
+                    socle.etages[etage_choisi[0]].add_rect(rect[i])
 
                 
                 
