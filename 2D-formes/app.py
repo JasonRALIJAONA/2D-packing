24fa33200ff83e11b594cf4d0e043bbfb8f60235
JasonRALIJAONA <jasonralijaona@gmail.com>
2024-07-14 07:45:39 -0900

    modification : cercle

---
+++ 2D-formes/app.py
@@ -0,0 +1,19 @@
+from shapely import Point, Polygon
+from util.Socle import Socle
+from util.Cercle import Cercle
+from util.Rectangle import Rectangle
+from model.Algorithme import Algorithme
+
+formes = []
+socle = Socle(0,0,100,100)
+
+formes.append(Cercle(1,25))
+formes.append(Cercle(2,25))
+formes.append(Cercle(3,25))
+formes.append(Cercle(4,25))
+
+algo = Algorithme()
+algo.brut_force(formes,socle)
+
+for forme in formes:
+    print(f"({forme.id}) : x : {forme.pos_x} , y : {forme.pos_y} , rayon : {forme.rayon}")
